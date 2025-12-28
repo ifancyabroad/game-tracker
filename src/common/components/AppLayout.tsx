b4bee702391f9f4d96a05905aa02977cd17fc2d4
Edgar Nightingale <edgar.nightingale@geniussports.com>
2025-12-28 17:06:02 +0000

    Scroll top top on nav change events

--- src/common/components/AppLayout.tsx
+++ src/common/components/AppLayout.tsx
@@ -1,14 +1,24 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { Sidebar } from "./Sidebar";
 import { Header } from "./Header";
+import { useEffect, useRef } from "react";
 
 export const AppLayout: React.FC = () => {
+	const mainRef = useRef<HTMLElement>(null);
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		if (mainRef.current) {
+			mainRef.current.scrollTop = 0;
+		}
+	}, [pathname]);
+
 	return (
 		<div className="flex h-screen flex-col bg-[var(--color-bg)] text-[var(--color-text)] sm:flex-row">
 			<Sidebar />
 			<div className="flex flex-1 flex-col overflow-hidden">
 				<Header />
-				<main className="flex-1 overflow-y-auto p-4 sm:p-6">
+				<main ref={mainRef} className="flex-1 overflow-y-auto p-4 sm:p-6">
 					<Outlet />
 				</main>
 			</div>
